7c2a9baf24302a12f9cb7f6612c4aad8290ae231
huzorro <huzorro@gmail.com>
2015-05-13 20:46:20 +0800

    添加数据结构

--- main.go
+++ main.go
@@ -5,12 +5,27 @@ import (
 )
 
 type Cfg struct {
-	TaskUri   string `json:"taskUri"`
-	SubmitUri string `json:"submitUri"`
-	RankPath  string `json:"rankPath"`
-	RankParam string `json:"rankParam"`
-	ThreadN   int64  `json:"threadN"`
-	Timeout   int64  `json:"timeout"`
+	TaskUri       string `json:"taskUri"`
+	SubmitUri     string `json:"submitUri"`
+	RankPath      string `json:"rankPath"`
+	RankParam     string `json:"rankParam"`
+	ThreadN       int64  `json:"threadN"`
+	Timeout       int64  `json:"timeout"`
+	Authorization string `json:"Authorization"`
+	ContentType   string `json:"Content-Type"`
+	Accept        string `json:"Accept"`
+}
+
+type TaskMsg struct {
+	Keyword     string `json:"keyword"`
+	Destlink    string `json:"destlink"`
+	Order       string `json:"order"`
+	Index       string `json:"index"`
+	InitTime    string `json:"initTime"`
+	ExecTime    string `json:"execTime"`
+	CostTime    string `json:"costTime"`
+	Status      string `json:"status"`
+	Description string `json:"description"`
 }
 
 func main() {
