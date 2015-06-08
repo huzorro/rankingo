612cffecf7aa5e6152ba8658c82fe70f5c4b45e2
huzorro <huzorro@gmail.com>
2015-06-08 19:38:15 +0800

    代理验证测试

--- httpx_test.go
+++ httpx_test.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
+	//	"io/ioutil"
+	_ "github.com/djimenez/iconv-go"
 	"testing"
 )
 
 func TestHttpGet(t *testing.T) {
-	resp, _ := HttpGet("https://www.oschina.net/home/login")
-	//	resp, _ := HttpGetFromProxy("http://ip138.com", "http://39.189.1.145:8123")
-	body, _ := ioutil.ReadAll(resp.Body)
+	//	resp, _ := HttpGet("https://www.oschina.net/home/login")
+	//		resp, _ := HttpGet("http://ip138.com")
+	resp, err := HttpGetFromProxy("http://www.ip.cn", "https://117.185.13.86:8006")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//	body, _ := ioutil.ReadAll(resp.Body)
+	//	utfBody, err := iconv.NewReader(resp.Body, "gbk", "utf-8")
+
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer resp.Body.Close()
-	fmt.Println(string(body))
+	if value, exists := doc.Find("#f_emailabc").Attr("name"); exists {
+		fmt.Println(value)
+	} else {
+		fmt.Println("proxy unable")
+	}
+	html := doc.Text()
+	fmt.Println(html)
 	fmt.Println("编码测试")
 }
